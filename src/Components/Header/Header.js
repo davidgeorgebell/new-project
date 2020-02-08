dc98b4eb46ffbedc9ff8f923e4a6ba749bd47d3b
davidgeorgebell <bell.dave93@gmail.com>
2020-02-08 10:44:57 +0800

    began header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,11 +5,13 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header-wrapper'>
-      <ul>
-        <li className='list-item-1'>Item 1</li>
-        <li className='list-item-2'>Item 2</li>
-        <li className='list-item-3'>Item 3</li>
-      </ul>
+      <nav>
+        <ul>
+          <li className='list-item-1'>Item 1</li>
+          <li className='list-item-2'>Item 2</li>
+          <li className='list-item-3'>Item 3</li>
+        </ul>
+      </nav>
     </div>
   );
 };
